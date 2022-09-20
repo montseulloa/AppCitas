e7475f2ed172750b65806b4c9b4ee21474917d40
Montse <montseullriv12345@gmail.com>
2022-09-20 07:34:57 -0500

    Create and return token

--- AppCitas.Service/Controllers/AccountController.cs
+++ AppCitas.Service/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using AppCitas.Service.Data;
 using AppCitas.Service.DTOs;
 using AppCitas.Service.Entities;
+using AppCitas.Service.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,13 +14,16 @@ public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
 
-    public AccountController(DataContext context)
+    private readonly ITokenService _tokenService;
+
+    public AccountController(DataContext context, ITokenService tokenService)
     {
         _context = context;
+        _tokenService = tokenService;
     }
 
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username))
             return BadRequest("Username is already taken!");
@@ -36,11 +40,15 @@ public class AccountController : BaseApiController
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
 
-        return user;
+        return new UserDto
+        {
+            UserName = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await _context.Users
             .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
@@ -58,7 +66,12 @@ public class AccountController : BaseApiController
                 return Unauthorized("Invalid username or password");
         }
 
-        return user;
+        return new UserDto
+        {
+            UserName = user.UserName,
+            Token = _tokenService.CreateToken(user)
+
+        };
     }
 
     #region Private methods
---
+++ AppCitas.Service/DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AppCitas.Service.DTOs
+{
+    public class UserDto
+    {
+        public string UserName { get; set; }
+        public string Token { get; set; }
+    }
+}
