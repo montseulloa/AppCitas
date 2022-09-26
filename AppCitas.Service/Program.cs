2cb7a2d3531fb0a80fd5e2352464ac820e19f2ae
Montse <montseullriv12345@gmail.com>
2022-09-26 07:35:11 -0500

    Seeding

--- AppCitas.Service/Program.cs
+++ AppCitas.Service/Program.cs
@@ -1,10 +1,29 @@
+using AppCitas.Service.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace AppCitas;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public async static Task Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var host = CreateHostBuilder(args).Build();
+        using var scope = host.Services.CreateScope();
+        var services = scope.ServiceProvider;
+
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            await context.Database.MigrateAsync();
+            await Seed.SeedUsers(context);
+        }
+        catch(Exception e)
+        {
+            var Logger = services.GetRequiredService<ILogger<Program>>();
+            Logger.LogError(e, "An error ocurred during migration/Seeding");
+        }
+
+        await host.RunAsync();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
