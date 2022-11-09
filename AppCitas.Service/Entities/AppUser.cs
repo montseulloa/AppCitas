c496b84f0f9a48d90389393c48c3e9face1db187
Montse <montseullriv12345@gmail.com>
2022-11-09 07:25:04 -0600

    Setting up automapper

--- AppCitas.Service/Entities/AppUser.cs
+++ AppCitas.Service/Entities/AppUser.cs
@@ -20,6 +20,8 @@ namespace AppCitas.Service.Entities
         public ICollection<Photo> Photos { get; set; }
         public ICollection<UserLike> LikedByUsers { get; set; }
         public ICollection<UserLike> LikedUsers { get; set; }
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
 
     }
 }
---
+++ AppCitas.Service/Interfaces/IMessageRepository.cs
@@ -0,0 +1,14 @@
+﻿using AppCitas.Service.DTOs;
+using AppCitas.Service.Entities;
+
+namespace AppCitas.Service.Interfaces;
+
+public interface IMessageRepository
+{
+    void AddMessage(Message message);
+    void DeleteMessage(Message message); 
+    Task<Message> GetMessage(int id);
+    Task<PagedList<MessageDto>> GetMessagesForUser();
+    Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+    Task<bool> SaveAllAsync();
+}
