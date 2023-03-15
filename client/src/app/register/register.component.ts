6e8ba4aa216447551d9cee21885953d42a7c91fe
Montse <montseullriv12345@gmail.com>
2023-03-15 07:12:25 -0600

    Client Side Validation 14/03/2023

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -10,18 +11,28 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService, private toastr: ToastrService) {}
 
   ngOninit(){
+    this.initializeForm();
+  }
 
+  initializeForm(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required])
+    });
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => this.cancel(),
-      error: error => this.toastr.error(error.error)
-    })
+    console.log(this.registerForm?.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => this.cancel(),
+    //   error: error => this.toastr.error(error.error)
+    // })
   }
 
   cancel(): void{
