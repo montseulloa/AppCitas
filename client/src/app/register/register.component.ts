d1b28d1c89ce4f1f06dbcc12e65e16129a9a888a
Montse <montseullriv12345@gmail.com>
2023-03-15 07:26:42 -0600

    Added custom validator 15/03/2023

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
@@ -8,14 +8,14 @@ import { AccountService } from '../_services/account.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
   registerForm: FormGroup = new FormGroup({});
 
   constructor(private accountService: AccountService, private toastr: ToastrService) {}
 
-  ngOninit(){
+  ngOnInit(): void {
     this.initializeForm();
   }
 
@@ -23,11 +23,20 @@ export class RegisterComponent {
     this.registerForm = new FormGroup({
       username: new FormControl('Hello', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required])
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
     });
   }
 
-  register() {
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : { notMatching : true } 
+    }
+  }
+
+  register(): void {
     console.log(this.registerForm?.value);
     // this.accountService.register(this.model).subscribe({
     //   next: () => this.cancel(),
@@ -35,7 +44,7 @@ export class RegisterComponent {
     // })
   }
 
-  cancel(): void{
+  cancel(): void {
     this.cancelRegister.emit(false);
   }
 }
